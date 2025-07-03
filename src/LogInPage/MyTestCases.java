fdd214745d4470e104acebc0d6fb5da7585c99fd
Maisrousan <maisrousan11@gmail.com>
2025-07-03 14:31:53 +0300

    update

--- src/LogInPage/MyTestCases.java
+++ src/LogInPage/MyTestCases.java
@@ -34,12 +34,43 @@ public class MyTestCases {
 		
 		driver.navigate().to(SignupPage);
 		
-		WebElement FirstName = driver.findElement(By.xpath("//*[@id=\"AccountFrm_firstname\"]"));
-		
+		WebElement FirstName = driver.findElement(By.xpath("//input[@id='AccountFrm_firstname']"));
+		WebElement LastName = driver.findElement(By.xpath("//input[@id='AccountFrm_lastname']"));
+		WebElement Email = driver.findElement(By.id("AccountFrm_email"));
+		WebElement Telephone = driver.findElement(By.id("AccountFrm_telephone"));
+		WebElement Fax = driver.findElement(By.id("AccountFrm_fax"));
+		WebElement Company = driver.findElement(By.id("AccountFrm_company"));
+		WebElement Address1 = driver.findElement(By.id("AccountFrm_address_1"));
+		WebElement Address2 = driver.findElement(By.id("AccountFrm_address_2"));
+		WebElement City = driver.findElement(By.id("AccountFrm_city"));
+		//WebElement Region = driver.findElement(By.id("AccountFrm_zone_id"));
+		WebElement ZIPCode = driver.findElement(By.id("AccountFrm_postcode"));
+		//WebElement Country = driver.findElement(By.id("AccountFrm_country_id"));
+		WebElement LoginName = driver.findElement(By.id("AccountFrm_loginname"));
+		WebElement Password = driver.findElement(By.id("AccountFrm_password"));
+		WebElement PasswordConfirm = driver.findElement(By.id("AccountFrm_confirm"));
+
+
 		FirstName.sendKeys("Mais");
+		LastName.sendKeys("Alrousan");
+		Email.sendKeys("maisrousan@gmail.com");
+		Telephone.sendKeys("87987638");
+		Fax.sendKeys("mm99");
+		Company.sendKeys("freema");
+		Address1.sendKeys("Irbid");
+		Address2.sendKeys("Irbid");
+		City.sendKeys("Irbid");
+		//Region.sendKeys("Irbid");
+		ZIPCode.sendKeys("0000");
+		//Country.sendKeys("Irbid");
+		LoginName.sendKeys("MaisRousan");
+		Password.sendKeys("Nn0000//");
+		PasswordConfirm.sendKeys("Nn0000//");
+		
+		
 	}
 
-	//Hi
+	
 
 	
 	
